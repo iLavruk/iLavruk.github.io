22426452f2c2c89abc3923f5aad5d69ced232dfa
iLavruk <ilavr8725@gmail.com>
2020-07-15 17:50:07 +0300

    js

---
+++ js-first-task/task_2.js
@@ -0,0 +1,7 @@
+var a, b;
+a = 10, b = 15;
+document.write("a = ", a + ", " + "b = ", b + '<br>');
+document.write("a + b = ", a + b + '<br>');
+document.write("a - b = ", a - b + '<br>');
+document.write("a * b = ", a * b + '<br>');
+document.write("a / b = ", a / b);
---
+++ js-first-task/task_3.js
@@ -0,0 +1,8 @@
+function Primer() {
+    var S, r, L;
+    r = forma.radius.value;
+    S = Math.PI * r * r;
+    L = 2 * Math.PI * r;
+    alert("Площа кола: " + S);
+    alert("Довжина кола: " + L);
+}
