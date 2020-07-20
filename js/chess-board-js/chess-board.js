2bcd206c73fb012914b4748999bddb8fdd4f0713
iLavruk <ilavr8725@gmail.com>
2020-07-20 15:58:16 +0300

    chees board

---
+++ js/chess-board-js/chess-board.js
@@ -0,0 +1,23 @@
+function chessBoard() {
+    var mainBlock = document.querySelector('.main-block');
+    var block;
+    var cell = true;
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            block = document.createElement('div');
+            if (j == 0) {
+                cell = !cell;
+            }
+            if (cell) {
+                block.className = 'block black';
+            }
+            else {
+                block.className = 'block white';
+            }
+            mainBlock.appendChild(block);
+            cell = !cell;
+        }
+    }
+
+}
+chessBoard();
